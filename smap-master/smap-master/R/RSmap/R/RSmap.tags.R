18f07b1048b58e39c9988ebfdf9952f94dd24d12
lichen_on_ubuntu_desktop <wulicheneason@gmail.com>
2022-05-13 09:25:36 -0600

    removed smap-nested

--- smap-master/smap-master/R/RSmap/R/RSmap.tags.R
+++
@@ -1,9 +0,0 @@
-RSmap.tags <-
-function(where){
-  query <- paste("select * where ", where)
-  res <- .RSmap.postQuery(query)
-  if (length(res)==0) {
-    write(paste("RSmap.tags: no tags found for streams where", where), stderr())
-  }
-  res
-}
--- smap-master/smap-master/python/smap/drivers/obvius/auth.py
+++
@@ -1,4 +0,0 @@
-ROOT = 'http://www.buildingmanageronline.com/members/'
-STATUSPAGE = 'client_status.php?DB=dbU216ucberkelF682'
-AUTH = ('ucbguest', 'ucbguest')
-BMOAUTH = AUTH
